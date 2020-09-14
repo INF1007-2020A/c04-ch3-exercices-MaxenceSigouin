cbdf438d5dae309c1a86bbf75586854ad9fd7d71
MaxenceSigouin <maxence.sigouin@polymtl.ca>
2020-09-14 23:36:45 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return 
+    return temperature - 32
 
 
 def to_farenheit(temperature: float) -> float:
