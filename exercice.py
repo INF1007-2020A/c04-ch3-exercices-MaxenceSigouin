62fef98cf4b58d807d99d90d49446a1c37e164d1
MaxenceSigouin <maxence.sigouin@polymtl.ca>
2020-09-14 22:31:34 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    to_radians = 
+    to_radians = (angle)
     return 0.0
 
 
